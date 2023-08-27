b8a0f5655d9c2c205295616a2c831b1ce58bfa4d
Md Moshiur Rahman <99rmhridoy@gmail.com>
2023-08-27 15:03:07 +0600

    second day commit

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -1,7 +1,15 @@
 'use client';
 import { AiOutlineMenu } from "react-icons/ai"
 import Avatar from "./Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 function UserMenu() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = useCallback(()=>{
+        setIsOpen((value) => !value);
+    },[]);
+
   return (
     <div className="relative">
         <div className="flex flex-row items-center gap-3">
@@ -23,7 +31,7 @@ function UserMenu() {
                 Airbnb your home
             </div>
             <div 
-            onClick={()=>{}}
+            onClick={toggleOpen}
             className="
                 p-4
                 md:py-1
@@ -46,6 +54,35 @@ function UserMenu() {
                 </div>
             </div>
         </div>
+        {isOpen && (
+            <div
+            className="
+                absolute
+                rounded-xl
+                shadow-md
+                w-[40vw]
+                md:w-3/4
+                bg-white
+                overflow-hidden
+                right-0
+                top-12
+                text-sm
+            "
+            >
+                <div className="flex flex-col cursor-pointer">
+                    <>
+                        <MenuItem 
+                        onClick={()=>{}}
+                        label="Login"
+                        />
+                        <MenuItem 
+                        onClick={()=>{}}
+                        label="Signup"
+                        />
+                    </>
+                </div>
+            </div>
+        )}
     </div>
   )
 }
